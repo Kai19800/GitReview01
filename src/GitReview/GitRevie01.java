b84e91f9c74c5e56278c525d81ea1858540cabad
Kai19800 <Kdc.2727@gmail.com>
2022-06-11 15:51:55 +0300

    3rd commit

--- src/GitReview/GitRevie01.java
+++ src/GitReview/GitRevie01.java
@@ -4,7 +4,20 @@ public class GitRevie01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
-		System.out.println("G");
+		String city = "I want to go to London";
+
+		System.out.println("I want to go to London");
+		System.out.println(city.replace("London", "Miami"));
+
+		String Str = "I love Java";
+		System.out.println(Str.replace("Java", "Python"));
+
+		String a = "Is it Saturday? Is it raining? Do we have a Java class today?";
+		String[] arr = a.split("[?]");
+		System.out.println(arr.length);}
+		
+	{
+		
 
 	}
 
